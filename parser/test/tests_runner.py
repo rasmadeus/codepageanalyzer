3246963086a34cfb5703c60eda1a3237891477bc
K. Kulikov <rasmadeus@gmail.com>
2015-08-07 22:12:24 +0300

    Remove old files

--- parser/test/tests_runner.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import unittest
-
-if __name__ == "__main__":
-    suite = unittest.TestLoader().discover(start_dir='../', pattern='test_*.py')
-    unittest.TextTestRunner(verbosity=2).run(suite)
--- src/test/tests_runner.py
+++
@@ -1,5 +0,0 @@
-import unittest
-
-if __name__ == "__main__":
-    suite = unittest.TestLoader().discover(start_dir='../', pattern='test_*.py')
-    unittest.TextTestRunner(verbosity=2).run(suite)
